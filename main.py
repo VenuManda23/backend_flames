fb022e0feb403567f7117a37e4623abdb99490fe
Venu Manda <venunaidu718@gmail.com>
2026-05-09 12:38:38 +0530

    backend commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import mysql.connector
+
+app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+db=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",
+    database="flamesdb"
+)
+cursor=db.cursor()
+
+class Flames(BaseModel):
+    name:str
+    partner_name:str
+    result:str
+@app.post('/flames')
+def save_data(data:Flames):
+    print(data)
+    query=""" insert into flames(name,partner_name,result) values(%s,%s,%s) """
+    values=(data.name,data.partner_name,data.result)
+    cursor.execute(query,values)
+    db.commit()
+    return{
+        "msg":"data saved successfully"
+    }
